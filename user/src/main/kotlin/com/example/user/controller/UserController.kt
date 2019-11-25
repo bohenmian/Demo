4bfffc8554e269f96d86b45e332474475176c24b
bohenmian <bohenmian@gmail.com>
2019-11-25 15:28:40 +0800

    fix stop shell error

--- user/src/main/kotlin/com/example/user/controller/UserController.kt
+++ user/src/main/kotlin/com/example/user/controller/UserController.kt
@@ -7,6 +7,7 @@ import com.example.user.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -20,6 +21,11 @@ class UserController(val userService: UserService) {
             ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body(userService.createUser(request))
+
+    @GetMapping("/api/user/hello")
+    fun hello(): String {
+        return "hello"
+    }
 
     @ExceptionHandler(UserNotExistException::class)
     fun handleUserNotExistException(e: UserNotExistException) =
